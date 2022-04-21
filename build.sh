11b2d8e2db657d521829684a0455b1d0ddbf075a
Lucien van Wouw <lucien.van.wouw@di.huc.knaw.nl>
2022-04-21 18:09:00 +0200

    forward slash mag in url aan het einde

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 
 version=$(git rev-parse master)
 tag=$(git describe --tags)
+tag='latest'
 #echo "<!-- ${tag} ${version} -->" > views/version.pug
 docker build --tag="registry.diginfra.net/lwo/iaa:${tag}" .
-docker push registry.diginfra.net/lwo/iaa:${tag}
+#docker push registry.diginfra.net/lwo/iaa:${tag}
 docker push registry.diginfra.net/lwo/iaa:latest
